2376ce25b5d9e2357b94a80bce4ad51c78ec1d30
FeniXb3 <fenix.b3@gmail.com>
2018-03-14 14:55:38 +0100

    Init commit

---
+++ src/com/codecool/java/geometry/shapes/Shape.java
@@ -0,0 +1,59 @@
+package com.codecool.java.geometry.shapes;
+
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * This is a abstract class representing geometrical shape.
+ */
+public abstract class Shape {
+    /**
+     * Calculates shape's area.
+     * @return area of the shape
+     */
+    public float getArea() {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Calculates shape's perimeter.
+     * @return perimeter of the shape
+     */
+    public float getPerimeter() {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Returns information about the shape as string.
+     * @return information bout shape
+     */
+    public String toString() {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Check if any of args are not below 0.
+     * Throws IllegalArgumentException if any of the parameters is below 0.
+     * @param args
+     * @return true if any of args are not below 0
+     */
+    public static boolean checkIfArgsNotBelowZero(float... args) {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Returns formula for the area of the shape as a string.
+     * @return area formula
+     */
+    public static  String getAreaFormula() {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Returns formula for the perimeter of the shape as a string.
+     * @return perimeter formula
+     */
+    public static String getPerimeterFormula() {
+        throw new NotImplementedException();
+    }
+}
