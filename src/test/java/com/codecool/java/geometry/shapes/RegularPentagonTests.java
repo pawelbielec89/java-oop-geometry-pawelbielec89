3f3c08fb13f13489f365a41a6d576a92189684af
FeniXb3 <fenix.b3@gmail.com>
2018-03-16 12:48:28 +0100

    Update test file name

--- src/test/java/com/codecool/java/geometry/shapes/RegularPentagonTests.java
+++ src/test/java/com/codecool/java/geometry/shapes/RegularPentagonTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class RegularPentagonTest {
+class RegularPentagonTests {
     float delta = 0.00001f;
 
     @Test
