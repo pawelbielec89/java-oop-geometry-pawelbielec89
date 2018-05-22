7b9988ae3fbb0c1682a2b45286f4c414859cfb47
Devmack <starzykdominik93@gmail.com>
2018-05-22 10:59:02 +0200

    Change getters for shape formulas to abstract to avoid unnesessary implementation in static context

--- src/main/java/com/codecool/java/geometry/shapes/Shape.java
+++ src/main/java/com/codecool/java/geometry/shapes/Shape.java
@@ -20,28 +20,24 @@ public abstract class Shape {
     public abstract double calculatePerimeter();
 
     /**
+     * Returns formula for the area of the shape as a string.
+     * @return area formula
+     */
+    public abstract String getAreaFormula();
+    
+    /**
+     * Returns formula for the perimeter of the shape as a string.
+     * @return perimeter formula
+     */
+    public abstract String getPerimeterFormula();
+
+    /**
      * Check if any of args are not below 0.
      * Throws IllegalArgumentException if any of the parameters is 0 or less.
      * @param args
      * @return true if any of args are greater than 0
      */
     public static boolean checkIfArgsGreaterThanZero(float... args) {
-        throw new NotYetImplementedException();
-    }
-
-    /**
-     * Returns formula for the area of the shape as a string.
-     * @return area formula
-     */
-    public static String getAreaFormula() {
-        throw new NotYetImplementedException();
-    }
-
-    /**
-     * Returns formula for the perimeter of the shape as a string.
-     * @return perimeter formula
-     */
-    public static String getPerimeterFormula() {
         throw new NotYetImplementedException();
     }
 }
